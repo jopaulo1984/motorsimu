0ed32e18723cbd4b628d55968fd9bcfc9bcd1dd3
jopaulo1984 <jopaulo1984@gmail.com>
2019-04-28 15:36:00 -0300

    atualização da versão
    Mudanças:
    - Criada a classe GraphicSerie que contém as informações da série de valores. Antes era necessário informar os arrays contendo os valores de cada série e outros arrays relativos às propriedades das séries. Agora, será necessário somente um array de objetos da classe GraphicSerie  cotendo o atributo points que são os pontos (x,y) do gráfico e os outros atributos como cor e rótulo da série.
    - O tamanho do graphic view ficou flexível, relativo à largura do objeto da classe LinearGraphic.

--- source/graficos.py
+++ source/graficos.py
@@ -3,7 +3,7 @@
 
 """
 +----------------------------------------+
-| graficos v0.1                          |
+| graficos v0.2                          |
 | Autor: João Paulo F da Silva           |
 | website: jpcompweb.com.br              |
 +----------------------------------------+
@@ -17,57 +17,99 @@ O `graficos` é um módulo que contém componentes para visualização gráfica 
 
 import tkinter as tk
 
-class GraficoLinha(tk.Canvas):
-    """Gráfico 2d que desenha os dados em linhas.
+class GraphicSerie:
+    def __init__(self,points=[(0,0)],x_div=1,y_div=1,x_label='X',y_label='Y',color='#FFFFFF'):
+        self.points = points
+        self.x_div = x_div
+        self.y_div = y_div
+        self.x_label = x_label
+        self.y_label = y_label
+        self.color = color
 
-    Uso
-    ===
-
-    Antes de desenhar, os atributos da classe devem ser definidos:
-      - series:  lista contendo várias outras listas de valores do eixo y do gráfico.
-      - x_init:  valor inicial do eixo x.
-      - x_div:   valor de cada divisão de x.
-      - x_label: rótulo do eixo x.
-      - y_div:   lista contendo o valor de cada divisão relativa ao índice da série de valores
-        definidos no atributo series.
-      - labels:  lista contendo o rótulo de cada série de valores.
-      - colors:  lista contendo a cor da linha de cada série de valores.
-      - y_0:     deslocamento em divisões do eixo y.
-      - x_0:     deslocamento em divisões do eixo x.
-
-    """
-
-    def __init__(self, *args, **keyargs):
-        super().__init__(*args, **keyargs)
-        self.series = []
-        self.x_init = 0
-        self.x_div = 0.1
-        self.x_label = 's'
-        self.y_div = []
-        self.labels = []
-        self.colors = []
-        self.backcolor = '#1A1A1A'
-        self.gridcolor = '#2D672D'
-        self.y_0 = 0
-        self.x_0 = 0
-        w, h = 700, 540
+class LineGraphic(tk.Canvas):
+    """Gráfico 2d que desenha os dados em linhas."""
+    
+    def __init__(self, master=None, series=[], w=740, h=560, *args, **keyargs):
+        super().__init__(master, *args, **keyargs)
+        self.__series = series
+        self.__y_0 = 0
+        self.__x_0 = 0
         self.config(width=w, height=h)
-        self.create_rectangle(0,0,w,h,fill=self.backcolor,outline='white')
-        self.create_rectangle(20,20,w-200,h-20,fill=self.backcolor,outline='#2DD500')
-        x = 30
-        while x < w-200:
+        self.set_colors('#1A1A1A','#008000','#1F571F','#0EAC0E')
+    
+    @property
+    def series(self):
+        return self.__series
+    
+    @series.setter
+    def series(self, value):
+        self.__series = value
+        self.draw()
+    
+    @property
+    def x_0(self):
+        return self.__x_0
+    
+    @property
+    def y_0(self):
+        return self.__y_0
+    
+    def set_size(self, w, h):
+        self.config(width=w, height=h)
+        self.__draw_all()
+    
+    def set_axes(self, x_0, y_0):
+        self.__x_0 = x_0
+        self.__y_0 = y_0
+        self.__draw_all()
+    
+    def set_colors(self, bg, div, subdiv, axes):
+        self.__bg = bg
+        self.__dcolor = div
+        self.__sdcolor = subdiv
+        self.__xycolor = axes
+        self.__draw_all()
+    
+    def __draw_base(self):
+        self.delete('base')
+        self.delete('series')
+
+        w = int(self.cget('width'))
+        h = int(self.cget('height'))
+        
+        gtop = 20
+        gleft = 20
+        gright = w - 200
+        gbottom = h - 20
+        gw = gright - gleft
+        gh = gbottom - gtop
+        
+        print(gw,gh)
+        
+        f = lambda i: int(i/10)
+        
+        self.create_rectangle(0,0,w,h,fill=self.__bg,outline='white', tags='base')
+        self.create_rectangle(gleft,gtop,gright,gbottom,fill=self.__bg,outline=self.__xycolor, tags='base')
+        
+        x = gleft + 10
+        for i in range(1, f(gw)):
             if x % 20 == 0:
-                self.create_line(x, 21, x, h-21, fill='#2E5922')
+                self.create_line(x, 21, x, h-21, fill=self.__dcolor, tags='base')
             else:
-                self.create_line(x, 21, x, h-21, fill='#183318')
+                self.create_line(x, 21, x, h-21, fill=self.__sdcolor, tags='base')
             x += 10
-        y = 30
-        while y < h-20:
+        
+        y = gtop + 10
+        for i in range(1, f(gh)):
             if y % 20 == 0:
-                self.create_line(21, y, w-201, y, fill='#2E5922')
+                self.create_line(21, y, w-201, y, fill=self.__dcolor, tags='base')
             else:
-                self.create_line(21, y, w-201, y, fill='#183318')
+                self.create_line(21, y, w-201, y, fill=self.__sdcolor, tags='base')
             y += 10
+    
+    def __draw_all(self):
+        self.__draw_base()
+        self.draw()
         
     def draw(self):
 
@@ -75,39 +117,24 @@ class GraficoLinha(tk.Canvas):
 
         w = int(self.cget('width'))
         h = int(self.cget('height'))
-
-        f_0 = lambda i: 20 + 10 * (25+i)
-
-        y_0 = f_0(-self.y_0)
-        x_0 = f_0(self.x_0)
-
-        self.create_line(20, y_0, w-200, y_0, fill='#2DD500',tags='series')
-        self.create_line(x_0, 20, x_0, h-20, fill='#2DD500',tags='series')
-
-        if len(self.series) == 0: return
-        def len_max():
-            m = 0
-            for serie in self.series:
-                if len(serie) > m: m = len(serie)
-            return m
-        
-        N = len_max()
-        if N == 0: return
-            
                 
-        gtop = 21
+        gtop = 20
         gleft = gtop
-        gright = w - 201
-        gbottom = h - 21
+        gright = w - 200
+        gbottom = h - 20
         gw = gright - gleft
         gh = gbottom - gtop
-        
-        g = lambda d: d + x_0
-        dx = gw / N if N > 0 else 0
-        x = self.x_init / self.x_div * 20
-        xant = x
+
+        f_0 = lambda i, j: 20 + 10 * ((j/2)+i)
+
+        y_0 = f_0(-self.y_0, gh / 10)
+        x_0 = f_0(self.x_0, gw / 10)
+
+        self.create_line(gleft, y_0, gright, y_0, fill='#2DD500',tags='series')
+        self.create_line(x_0, gtop, x_0, gbottom, fill='#2DD500',tags='series')
+                
         f = lambda value, y_div: y_0 - (20/y_div) * value
-        
+        g = lambda value, x_div: x_0 + (20/x_div) * value
 
         def get_limited_points(p1,p2):
             def _f(i1,i2,l1,l2):
@@ -125,48 +152,56 @@ class GraficoLinha(tk.Canvas):
             y1,y2 = _f(y1,y2,gtop,gbottom)
             if y1 is None: return None,None,None,None
             return x1,y1,x2,y2
+                   
+        xl0 = w-180
+        xl1 = xl0+5
+        xl2 = xl1+20
+        xl3 = xl2+5
+        yl0 = 0
         
-        for i in range(0, N):
-            if i > 0:
-                for j, serie in enumerate(self.series):
-                    if self.y_div[j] == 0: continue
-                    x1,y1,x2,y2 = get_limited_points((g(xant),f(serie[i-1], self.y_div[j])), (g(x),f(serie[i], self.y_div[j])))
-                    if x1 is None: continue
-                    self.create_line(x1, y1, x2, y2, fill=self.colors[j], width=1, tags='series')
-            xant = x
-            x += dx
-            
-        labels = [(label, i) for i, label in enumerate(self.labels) if self.y_div[i] > 0]
-        x0 = w - 180
-        x1 = x0+5
-        x2 = x1+20
-        x3 = x2+5
-        y0 = 0
+        def fleg(leg):
+            result = ''
+            for i in range(20):
+                if i < len(leg):
+                    result += leg[i]
+                else:
+                    result += ' '
+            return result
         
-        for label, i in labels:
-            if self.y_div[i] == 0: continue
-            y = y0 * 20 + 35
-            y0+=1
-            self.create_line(x1, y, x2, y, fill=self.colors[i], width=4, tags='series')
-            self.create_text(x3, y - 10, text='%s (%0.2f/div)' % (label,self.y_div[i]), tags='series', anchor=tk.NW, fill='white')
+        for serie in self.series:
+            if not type(serie) is GraphicSerie: continue
+            if 0 in (serie.y_div, serie.x_div): continue
+            #== curva ==
+            for i in range(1,len(serie.points)):
+                x1, y1 = serie.points[i-1]
+                x2, y2 = serie.points[i]
+                x1,y1,x2,y2 = get_limited_points((g(x1, serie.x_div),f(y1, serie.y_div)), (g(x2, serie.x_div),f(y2, serie.y_div)))
+                if x1 is None: continue
+                self.create_line(x1, y1, x2, y2, fill=serie.color, width=1, tags='series')
+                
+            #== legenda ==
+            y = yl0 * 60 + 35
+            yl0 += 1
+            self.create_line(xl1, y, xl2, y, fill=serie.color, width=4, tags='series')
+            self.create_text(xl3, y - 10, font=('Courier New', 9), text='%s\nx=%0.2f/div\ny=%0.2f/div\n' % (fleg(serie.y_label),serie.x_div,serie.y_div), tags='series', anchor=tk.NW, fill='white')
         
-        self.create_text(w-200,h-5, text='%0.2f%s' % (self.x_div * 25, self.x_label), anchor=tk.SW, tags='series', fill='white')
-        
-def get_graphico(title,series, x_max, x_div, x_label, colors, y_div, labels,x_0=0,y_0=0):
+def get_graphico(title,series,x_0=0,y_0=0):
     win = tk.Tk()
     win.title(title)
-    gr = GraficoLinha(win)
+    gr = LineGraphic(win,series)
     gr.pack(fill=tk.BOTH)
-    gr.series = series
-    gr.x_label = x_label
-    gr.x_max = x_max
-    gr.x_div = x_div
-    gr.y_div = y_div
-    gr.colors = colors
-    gr.labels = labels
-    gr.x_0 = x_0
-    gr.y_0 = y_0
-    gr.draw()
+    gr.set_axes(x_0,y_0)
     win.mainloop()
     return gr
+
+if __name__ == "__main__":
+    import math
+    f = lambda i: i * 0.1
+    g = lambda x: (x,x**2+x+1)
+    h = lambda x: (x,math.tan(x))
+    
+    serie1 = GraphicSerie([g(f(i)) for i in range(-100,100)],1,1,'s','f(x)= x² + x + 1','#0000FF')
+    serie2 = GraphicSerie([h(f(i)) for i in range(-10,10)],0.1,0.1,'s','g(x)= tan(x)','#FFFF00')
+    
+    get_graphico('Math Graphic',[serie1,serie2])
         
